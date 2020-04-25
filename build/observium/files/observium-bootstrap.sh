ad4c220505ad1f8410a259b2a38148caa5658b1f
Matt Robey <matt@r0bey.co.uk>
2020-04-25 23:03:49 +0100

    Do not bootstrap if data already exists

--- build/observium/files/observium-bootstrap.sh
+++ build/observium/files/observium-bootstrap.sh
@@ -6,13 +6,15 @@ until /usr/bin/mysql -u ${MYSQL_OBSERVIUM_USERNAME} -h mysql -p${MYSQL_OBSERVIUM
     sleep 5
 done
 
-echo "[ Observium Bootstrap ] - Setup database.."
-/usr/bin/php /opt/observium/discovery.php -u
+if ! /usr/bin/mysql -u ${MYSQL_OBSERVIUM_USERNAME} -h mysql -p${MYSQL_OBSERVIUM_PASSWORD} -e "select 1 from ${MYSQL_OBSERVIUM_DATABASE}.users;"; then
+    echo "[ Observium Bootstrap ] - Setup database.."
+    /usr/bin/php /opt/observium/discovery.php -u
 
-echo "[ Observium Bootstrap ] - Create ${OBSERVIUM_ADMIN_USERNAME} user.."
-/usr/bin/php /opt/observium/adduser.php ${OBSERVIUM_ADMIN_USERNAME} ${OBSERVIUM_ADMIN_PASSWORD} 10
+    echo "[ Observium Bootstrap ] - Create ${OBSERVIUM_ADMIN_USERNAME} user.."
+    /usr/bin/php /opt/observium/adduser.php ${OBSERVIUM_ADMIN_USERNAME} ${OBSERVIUM_ADMIN_PASSWORD} 10
 
-echo "[ Observium Bootstrap ] - Add demo device.."
-/usr/bin/php /opt/observium/add_device.php demo.snmplabs.com public
+    echo "[ Observium Bootstrap ] - Add demo device.."
+    /usr/bin/php /opt/observium/add_device.php demo.snmplabs.com public
+fi
 
 echo "[ Observium Bootstrap ] - Complete"
